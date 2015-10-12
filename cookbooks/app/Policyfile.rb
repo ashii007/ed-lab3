2a775930760481c98871f9d3e22f70f9dc2edda6
Joel Boekankamp <joel.boekankamp@gmail.com>
2015-10-12 19:55:07 +0000

    adding rubocop and testing configs

--- cookbooks/app/Policyfile.rb
+++ cookbooks/app/Policyfile.rb
@@ -4,13 +4,13 @@
 # https://github.com/opscode/chef-dk/blob/master/POLICYFILE_README.md
 
 # A name that describes what the system you're building with Chef does.
-name "app"
+name 'app'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list "app::default"
+run_list 'app::default'
 
 # Specify a custom source for a single cookbook:
-cookbook "app", path: "."
+cookbook 'app', path: '.'
--- cookbooks/app/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/app/test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'app::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'httpd service is running' do
+    expect(service 'httpd').to be_running
   end
 end
